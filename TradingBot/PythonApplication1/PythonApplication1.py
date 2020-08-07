c76b62c9652fa6f84334b983f8664baf389c45cb
Tony Virgin <vir16007@byui.edu>
2020-08-07 17:41:40 -0600

    added to the branch

--- TradingBot/PythonApplication1/PythonApplication1.py
+++ TradingBot/PythonApplication1/PythonApplication1.py
@@ -1,1 +1,16 @@
+import alpaca_trade_api as tradeapi
 
+class PythonBot:
+    def _int_(self):
+        self.alpaca = tradeapi.rest(APCA_API_KEY_ID, APCA_API_SECRET_KEY, APCA_API_BASE_URL, api_version='v2')
+
+    def run(self) :
+        #each minute
+        async def on_minute(conn, channel, bar):
+            if bar.close >= bar.open and bar.open - bar.low > 0.1:
+                print("Buying stock")
+                self.alpaca.submit_order("MSFT", 1, 'buy', 'market', 'day')
+                # take profit here
+
+bd = PythonBot()
+bd.run()
